83998431d4cec81dbdccff5f67929055a220a19e
umemotomarina <c0055@oic.jp>
2022-05-13 10:41:59 +0900

    9-1まで

--- Project/GameApp.cpp
+++ Project/GameApp.cpp
@@ -9,6 +9,14 @@
 
 //INCLUDE
 #include	"GameApp.h"
+#include	"Player.h"
+
+CCamera		gCamera;
+CDirectionalLight	gLight;
+CPlayer				gPlayer;
+bool				gbDebug = false;
+
+
 
 /*************************************************************************//*!
 		@brief			アプリケーションの初期化
@@ -20,6 +28,21 @@
 MofBool CGameApp::Initialize(void){
 	//リソース配置ディレクトリの設定
 	CUtilities::SetCurrentDirectory("Resource");
+
+	gCamera.SetViewPort();
+	gCamera.LookAt(Vector3(0, 6.0f, -17.0f), Vector3(0, 0, -10), Vector3(0, 1, 0));
+	gCamera.PerspectiveFov(MOF_ToRadian(60.0f), 1024.0f / 768.0f, 0.01f, 1000.0f);
+	CGraphicsUtilities::SetCamera(&gCamera);
+
+	gLight.SetDirection(Vector3(-1, -2, 1.5f));
+	gLight.SetDiffuse(MOF_COLOR_WHITE);
+	gLight.SetAmbient(MOF_COLOR_HWHITE);
+	gLight.SetSpeculer(MOF_COLOR_WHITE);
+	CGraphicsUtilities::SetDirectionalLight(&gLight);
+
+	gPlayer.Load();
+	gPlayer.Initialize();
+
 	
 	return TRUE;
 }
@@ -33,6 +56,22 @@ MofBool CGameApp::Initialize(void){
 MofBool CGameApp::Update(void){
 	//キーの更新
 	g_pInput->RefreshKey();
+
+	gPlayer.Update();
+
+	if (g_pInput->IsKeyPush(MOFKEY_F1))
+	{
+		gbDebug = ((gbDebug) ? false : true);
+	}
+
+	float posX = gPlayer.GetPosition().x * 0.4f;
+	CVector3 cpos = gCamera.GetViewPosition();
+	CVector3 tpos = gCamera.GetTargetPosition();
+	CVector3 vup = CVector3(0, 1, 0);
+	cpos.x = posX;
+	tpos.x = posX;
+	gCamera.LookAt(cpos, tpos, vup);
+	gCamera.Update();
 	return TRUE;
 }
 
@@ -49,6 +88,24 @@ MofBool CGameApp::Render(void){
 	// 画面のクリア
 	g_pGraphics->ClearTarget(0.65f,0.65f,0.67f,0.0f,1.0f,0);
 
+	g_pGraphics->SetDepthEnable(TRUE);
+
+	gPlayer.Render();
+
+	if (gbDebug)
+	{
+		CMatrix44 matWorld;
+		matWorld.Scaling(FIELD_HALF_X * 2, 1, FIELD_HALF_Z * 2);
+		CGraphicsUtilities::RenderPlane(matWorld, Vector4(1, 1, 1, 0.4f));
+	}
+
+	g_pGraphics->SetDepthEnable(FALSE);
+
+	if (gbDebug)
+	{
+		gPlayer.RenderDebugText();
+	}
+
 	// 描画の終了
 	g_pGraphics->RenderEnd();
 	return TRUE;
@@ -61,5 +118,6 @@ MofBool CGameApp::Render(void){
 						それ以外	失敗、エラーコードが戻り値となる
 *//**************************************************************************/
 MofBool CGameApp::Release(void){
+	gPlayer.Release();
 	return TRUE;
 }
